5c9e46bae8046bf1edfb69f2f98aefd694abd850
rafael.moura <rafaelfelipe.peixoto@gmail.com>
2022-10-31 08:46:47 -0300

    Criando lista de estudo mockada

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Formulario from "./components/Formulario";
+import Lista from "./components/Lista";
 
 function App() {
   return (
     <div className="App">
       <h1>Sistema de Login</h1>
       <Formulario />
+      <Lista />
     </div>
   );
 }
